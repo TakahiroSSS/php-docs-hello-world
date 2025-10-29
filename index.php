0817d9db63168f1da2a50dd08ac39232062b6cd9
TakahiroSSS <nhs50673@nhs.hal.ac.jp>
2025-10-29 10:43:52 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 
-echo "Hello World! 2025/10/29 10:33編集\n";
-echo "10:35 編集3回目\n";
-echo "10:39 編集4回目\n";
+echo "Hello World! 2025/10/29 10:33編集<br>";
+echo "10:35 編集3回目<br>";
+echo "10:39 編集4回目<br>";
+echo "10:43 編集5回目<br>";
